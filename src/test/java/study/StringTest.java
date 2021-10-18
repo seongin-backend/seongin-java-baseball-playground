6f5b68c23a43c2d626367b8102bf0c7631dbfb63
rom <alterin@naver.com>
2021-10-18 16:14:00 +0900

    21312

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -6,9 +6,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
-    void replace() {
-        //clone test2
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");w
+    void split() {
+
+        //0. 학습테스트실습
+
+        /*요구사항1
+        "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+        "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        */
+
+        /**********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.***********/
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).containsExactly("1", "2");
+        /********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다. 끝***********/
+
+        /********"1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.***********/
+        actual = "1".split(",");
+        assertThat(actual).contains("1");
+        /********"1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다. 끝**********/
+
+    }
+
+    @Test
+    void substring() {  231312
+
+        //0. 학습테스트실습
+
+        /*요구사항1
+        "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
+        "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
+        */
+
+        /**********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.***********/
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).containsExactly("1", "2");
+        /********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.***********/
+
+        /********"1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.***********/
+        actual = "1".split(",");
+        assertThat(actual).contains("1");
+        /********"1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.**********/
+
     }
 }
