d2a93008b121606a9a221646dacf3125a4ea3d89
SEONGIN <alterin@naver.com>
2022-12-29 11:09:09 +0900

    Set Test Case 작성 연습

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -1,11 +1,17 @@
 package study;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SetTest {
     private Set<Integer> numbers;
@@ -23,5 +29,26 @@ public class SetTest {
     @Test
     void checkHashSetSize() {
         assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("JUnit의 ParameterizedTest을 활용한 반복 최소화")
+    void containsAtParameterizedTest(int number) {//JUnit의 ParameterizedTest을 활용한 반복 최소화
+        assertTrue(numbers.contains(number));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,true","2,true","3,true","4,false","5,false"})
+    @DisplayName("1,2,3이면 true 4 ,5면 false")
+    void containsAtParameterizedTestAndCsvSource(int input, boolean expected) {//1,2,3이면 true 4 ,5면 false
+        assertEquals(expected, numbers.contains(input));
     }
 }
