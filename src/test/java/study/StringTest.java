60280afe8df2573eeb45fe814576bbf38741bd82
SEONGIN <alterin@naver.com>
2022-12-29 14:00:28 +0900

    StringIndexOutOfBoundsException 에러 발생시키고 테스트 케이스  작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -9,17 +9,13 @@ import static org.assertj.core.api.Assertions.*;
 public class StringTest {
     @Test
     void split() {
-
         //0. 학습테스트실습
-
         /*요구사항1
         "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
         "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
         */
-
         /**********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.***********/
         String[] actual = "1,2".split(",");
-
         assertThat(actual).containsExactly("1", "2");
         /********"1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다. 끝***********/
 
@@ -35,21 +31,20 @@ public class StringTest {
         /*요구사항 2*/
         /*"(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.*/
         String str = "(1,2)";
-
         str = str.substring(1,str.length()-1);
-        System.out.println(str);
         assertThat(str).contains("1,2");
     }
 
     @Test
-    @DisplayName("StringIndexOutOfBoundsException 에러에 대한 Exception")
+    @DisplayName("StringIndexOutOfBoundsException 에러 발생시키고 Exception 테스트")
     void charAt() {
         /*charAt 특정위치찾기*/
         String str = "abc";
+        int index = 3;
         assertThatThrownBy(() -> {
-            str.charAt(3);
-        }).isInstanceOf(IndexOutOfBoundsException.class)
-                .hasMessageContaining("Index: 2, Size: 2");
+            char c = str.charAt(index);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("%d", index);
 
     }
 
