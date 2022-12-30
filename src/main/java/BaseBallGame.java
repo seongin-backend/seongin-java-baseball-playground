03d9ba572cdd22d24d7d60afd31db6d6f5383d36
SEONGIN <alterin@naver.com>
2022-12-30 13:28:08 +0900

    진행중

--- src/main/java/BaseBallGame.java
+++ src/main/java/BaseBallGame.java
@@ -2,33 +2,49 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
-import java.util.stream.IntStream;
 
 public class BaseBallGame {
     String answer = "";
     String input = "";
+
     public String chooseAnswer() {
         Random random = new Random();
         while (answer.length() < 3) {
-            int nextInt = random.nextInt(9)+1; //1부터 9까지
-            if(answer.contains(String.valueOf(nextInt))) continue; //중복안되게 다시 숫자 뽑음
+            int nextInt = random.nextInt(9) + 1; //1부터 9까지
+            if (answer.contains(String.valueOf(nextInt))) continue; //중복안되게 다시 숫자 뽑음
             answer = answer + nextInt;
         }
         return answer;
     }
+
     public String chooseInput() throws IOException { //사용자가 입력한 숫자
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("숫자를 입력해 주세요 : ");
         String readLine = bufferedReader.readLine();
         return readLine;
     }
+
     public String compareAnswerAndInput(String answer, String input) {
         int intStrike = 0;
         int intBall = 0;
         int intEct = 0;
-        for (int i = 0; i < input.length(); i++) {
-            //if(answer.indexOf(answer.charAt(i)) == i)
+        for (int i = 0; i < answer.length(); i++) {
+            if(answer.charAt(i)==input.charAt(i)) intStrike++;
+            if(answer.charAt(i)!=input.charAt(i)) if(input.contains(String.valueOf(answer.charAt(i)))) intBall++;
         }
-        return "";
+        if(intBall>0 && intStrike>0) { System.out.println(intBall+"볼 "+intStrike+"스트라이크"); return "Fail";}
+        if(intStrike>0) {
+            if(intStrike == 3) return "Success";
+            System.out.println(intStrike+"스트라이크");
+            return "Fail";
+        }
+        if(intBall>0) { System.out.println(intBall+"볼 "); return "Fail";}
+        System.out.println("낫싱");
+        return "Fail";
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
--- src/test/java/BaseBallGameTest.java
+++ src/test/java/BaseBallGameTest.java
@@ -2,11 +2,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseBallGameTest {
     BaseBallGame baseBallGame;
-
+    String answer = "";
+    String input = "";
     @BeforeEach
     void setUp() {
         baseBallGame = new BaseBallGame();
@@ -15,12 +18,21 @@ public class BaseBallGameTest {
     @Test
     @DisplayName("정답 매긴거 중복된 값 있는지 체크")
     void DuplicateCheck() {
-        String answer = baseBallGame.chooseAnswer();
-        Boolean expect;
+        answer = baseBallGame.chooseAnswer();
         int cnt = 0;
         for (int i = 0; i < answer.length(); i++) {
             if(answer.indexOf(answer.charAt(i)) != i) cnt++; //indexOf가 일치하는 첫 index 내뱉으니까 안 일치하면 중복된거
         }
         assertThat(cnt).isZero();
+    }
+
+    @Test
+    void test() throws IOException {
+        String result = "Fail";
+        answer = baseBallGame.chooseAnswer();
+        input = baseBallGame.chooseInput();
+        while (result.equals("Fail")) {
+            result = baseBallGame.compareAnswerAndInput(answer, input);
+        }
     }
 }
